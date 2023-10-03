2e1e0e483713788645383900f021c24def94d127
IrinaMuravyova <miomir@yandex.ru>
2023-10-03 13:11:31 +0800

    Initial Commit

---
+++ PersonalQuize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuize
+//
+//  Created by Irina Muravyeva on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
