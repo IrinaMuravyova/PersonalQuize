9855c2419a1d44cd926136c3d69a2c7598f5add9
IrinaMuravyova <miomir@yandex.ru>
2023-10-08 19:42:49 +0800

    оптимизирован поиск часто встречающегося животного

--- PersonalQuize/ViewControlls/ResultViewController.swift
+++ PersonalQuize/ViewControlls/ResultViewController.swift
@@ -14,7 +14,6 @@ class ResultViewController: UIViewController {
     
     var userAnswers: [Answer]!
     
-    private var animalsCount: [Animal: Int] = [:]
     private var yourAnimal: Animal!
 
     override func viewDidLoad() {
@@ -30,27 +29,28 @@ class ResultViewController: UIViewController {
 
     private func calculateResult () {
         
-        for answer in userAnswers {
-            switch answer.animal {
-            case .cat:
-                if animalsCount[.cat] == nil {animalsCount = [.cat: 0]}
-                animalsCount[.cat]! += 1
-            case .dog:
-                if animalsCount[.dog] == nil {animalsCount = [.dog: 0]}
-                animalsCount[.dog]! += 1
-            case .rabbit:
-                if animalsCount[.rabbit] == nil {animalsCount = [.rabbit: 0]}
-                animalsCount[.rabbit]! += 1
-            case .turtle:
-                if animalsCount[.turtle] == nil {animalsCount = [.turtle: 0]}
-                animalsCount[.turtle]! += 1
-                
-            }
-         }
+        var animalsCount: [Animal: Int] = [:]
+        let animals = userAnswers.map({$0.animal})
         
-        print(animalsCount)
+        for animal in animals {
+            if let animalCount = animalsCount[animal] {
+                animalsCount.updateValue(animalCount + 1,
+                                         forKey: animal)
+            } else {
+                animalsCount[animal] = 1
+            }
+        }
+// второй вариант цикла
+//        for animal in animals {
+//            animalsCount[animal] = (animalsCount[animal] ?? 0) + 1
+//        }
+//
+//        третий вариант
+//        for animal in animals {
+//            animalsCount[animal, default: 0] += 1
+//        }
+        
         let animalsCountSorted = animalsCount.sorted {$0.value > $1.value}
-        print(animalsCountSorted)
         yourAnimal = animalsCountSorted.first?.key
     }
     
